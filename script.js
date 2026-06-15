863a8a45829abf1157a6c07eefe15112492e5301
Zain-Ul-Abedin <zainulabedin.remote@gmail.com>
2026-06-16 01:16:14 +0500

    UI/UX Overhaul to Cosmic Chess

--- script.js
+++ script.js
@@ -18,10 +18,20 @@ let timerStopped = true;
 let lastTick = null;
 let timerHandle = null;
 
-// Unicode chess pieces
-const piecesMap = {
-    'p': '♟', 'n': '♞', 'b': '♝', 'r': '♜', 'q': '♛', 'k': '♚',
-    'P': '♙', 'N': '♘', 'B': '♗', 'R': '♖', 'Q': '♕', 'K': '♔'
+// SVG chess pieces from Wikipedia Commons
+const pieceImages = {
+    'p': 'https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg',
+    'n': 'https://upload.wikimedia.org/wikipedia/commons/e/ed/Chess_ndt45.svg',
+    'b': 'https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg',
+    'r': 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg',
+    'q': 'https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg',
+    'k': 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg',
+    'P': 'https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg',
+    'N': 'https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg',
+    'B': 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg',
+    'R': 'https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg',
+    'Q': 'https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg',
+    'K': 'https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg'
 };
 
 function formatTime(totalSeconds) {
@@ -124,8 +134,9 @@ function renderBoard() {
             // Place piece
             const piece = board[r][f];
             if (piece) {
-                const pieceSymbol = piece.color === 'w' ? piecesMap[piece.type.toUpperCase()] : piecesMap[piece.type];
-                squareEl.innerHTML = `<span class="piece">${pieceSymbol}</span>`;
+                const pieceKey = piece.color === 'w' ? piece.type.toUpperCase() : piece.type;
+                const pieceUrl = pieceImages[pieceKey];
+                squareEl.innerHTML = `<div class="piece" style="background-image: url('${pieceUrl}');"></div>`;
             }
 
             // Add interaction
